94c5606c756f7650137ea44a3bdbf50b3a08735b
guelaoismael <guelaoismael@gmail.com>
2024-09-20 16:26:03 +0100

    add match_case_calculator.py file

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter the frist number: "))
+num2 = int(input("Enter the second number: "))
+
+operation = input("Choose the operation (+, -, *, /): ")
+
+result = 0
+
+match operation:
+    case "+":
+       print(f"The result is {num1 + num2}")
+    case "-":
+        print(f"The result is {num1 - num2}")
+    case "*":
+        print(f"The result is {num1 * num2}")
+    case "/":
+        if num2 == 0 :
+            print("Cannont divide by zero")
+        else:
+            print(f"The result is {num1 / num2}")
+    case _:
+        print("Invalid operation")
+    
