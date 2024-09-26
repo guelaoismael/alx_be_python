0fa018f7d354d29dcbd85fd77f85124c9ec5c8c8
guelaoismael <109024036+guelaoismael@users.noreply.github.com>
2024-09-26 17:06:54 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return -1
-            elif:
+            elif num2 != 0:
                 return num1 / num2
         case _:
             print("Cannot recognize this operation")
