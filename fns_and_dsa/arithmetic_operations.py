60e545831ccd622b7af049350286b3a50af78b26
guelaoismael <guelaoismael@gmail.com>
2024-09-26 17:59:20 +0100

    update arithmetic_operations.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,14 +2,14 @@ def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             return num1 + num2
-        case "substract":
+        case "subtract":
             return num1 - num2
         case "multiply":
             return num1 * num2
         case "divide":
             if num2 == 0:
                 return -1
-            else:
+            elif num2 != 0:
                 return num1 / num2
         case _:
             print("Cannot recognize this operation")
