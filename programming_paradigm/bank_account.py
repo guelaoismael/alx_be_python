6679a26815494191d0b09196e93b6b2267f48b2e
guelaoismael <guelaoismael@gmail.com>
2024-10-02 16:54:25 +0100

    add bank_account.py file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance}"
+        print(f"Current Balance: ${self.account_balance}")
