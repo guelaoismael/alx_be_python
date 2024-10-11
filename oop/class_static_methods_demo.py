e89cc61b08b5c5af92167b990d9b687458ccd84d
guelaoismael <guelaoismael@gmail.com>
2024-10-11 17:20:04 +0100

    Add class static methods file

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -3,11 +3,11 @@ class Calculator:
 
      # This static method named "add" takes two numbers as input and returns their sum.
     @staticmethod
-    def add(num1, num2):
-        return num1 + num2
+    def add(a, b):
+        return a + b
 
     # This Python class method `multiply` takes two numbers as input and returns their product, while also printing the calculation type.
     @classmethod 
-    def multiply(cls, num1, num2):
+    def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
-        return num1 * num2
+        return a * b
